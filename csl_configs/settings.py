5e6a271c9294ce2d4bf4b266ca7808191bd81d38
Lozano0916 <jairoedo2@gmail.com>
2023-10-24 00:53:12 -0500

    implementacion de la platilla html

--- csl_configs/settings.py
+++ csl_configs/settings.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -122,6 +123,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'csl_app/static'),
+    os.path.join(BASE_DIR, 'csl_app/static/img'),
+    os.path.join(BASE_DIR, 'csl_app/static/js'),
+
+]
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
