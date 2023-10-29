f5ac1f535bca37ea2ceb8413f7e6b43a1f58c1ce
Lozano0916 <jairoedo2@gmail.com>
2023-10-29 11:00:52 -0500

    login y seccion  de producto

--- csl_app/urls.py
+++ csl_app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import Inicio
+from .views import Inicio, Login, Product
 
-urlpatterns=[path('', Inicio.as_view(), name='inicio'),
+urlpatterns=[
+    path('inicio', Inicio.as_view(), name='inicio'),
+    path('login', Login.as_view(), name='login'),
+    path('product', Product.as_view(), name='product'),
 ]
--- csl_app/views.py
+++ csl_app/views.py
@@ -5,3 +5,9 @@ from django.views.generic import TemplateView
 # Create your views here.
 class Inicio(TemplateView):
     template_name = 'index.html'
+
+class Login(TemplateView):
+    template_name = 'login.html'
+
+class Product(TemplateView):
+    template_name = 'product.html'
